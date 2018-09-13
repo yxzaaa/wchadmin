0382c19b9d3bfca6d3cb0648220ccc6ee2ca3975
yxzaaa <2280517552@qq.com>
2018-09-13 17:07:19 +0800

    1

--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Resource from 'vue-resource'
 import IndexPage from '@/components/Index'
-import MainInfo from '@/components/MainInfo'
 import Login from '@/components/Login'
 import Register from '@/components/Register'
-import AccountCenter from '@/components/AccountCenter'
-import ActivePage from '@/components/ActivePage'
-import BeiJing from '@/components/BeiJing'
-import ChongQing from '@/components/ChongQing'
 import PageAdmin from '@/components/PageAdmin'
 import UserAdmin from '@/components/UserAdmin'
 import PayAdmin from '@/components/PayAdmin'
@@ -24,27 +19,7 @@ export default new Router({
       children:[
         {
           path:'/',
-          component:MainInfo
-        },
-        {
-          path:'/maininfo',
-          component:MainInfo
-        },
-        {
-          path:'/account',
-          component:AccountCenter
-        },
-        {
-          path:'/active',
-          component:ActivePage
-        },
-        {
-          path:'/beijing',
-          component:BeiJing
-        },
-        {
-          path:'/chongqing',
-          component:ChongQing
+          component:PageAdmin
         },
         {
           path:'/pageadmin',
@@ -66,27 +41,7 @@ export default new Router({
       children:[
         {
           path:'/',
-          component:MainInfo
-        },
-        {
-          path:'/maininfo',
-          component:MainInfo
-        },
-        {
-          path:'/account',
-          component:AccountCenter
-        },
-        {
-          path:'/active',
-          component:ActivePage
-        },
-        {
-          path:'/beijing',
-          component:BeiJing
-        },
-        {
-          path:'/chongqing',
-          component:ChongQing
+          component:PageAdmin
         },
         {
           path:'/pageadmin',
